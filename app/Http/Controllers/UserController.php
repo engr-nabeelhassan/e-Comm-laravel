2803c50dcfaf700f39869b7d8b4627cb415dcffb
Nabeel Ahmed <nabeelkhoso46@gmail.com>
2025-04-23 21:44:58 +0500

    login done

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+Use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+
+class UserController extends Controller
+{
+    //
+    function login(Request $req){
+        $user = User::where(['email'=>$req->email])->first();
+        if(!$user || !Hash::check($req->password, $user->password)){
+            return "Email Or Password In Not Matched";
+        }else{
+            $req->session()->put('user',$user);
+            return redirect('/');
+        }
+    }
+}
